b541b4c021fdbdb3695c3aaca369527826d773b4
manojeminem <35653185+manojeminem@users.noreply.github.com>
2018-01-30 15:46:46 +0530

    Create man.c

---
+++ man.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main()
+{
+   int a,b,c;
+a=1,b=2,c=3;
+    printf("Enter three different numbers: ");
+    scanf("%lf %lf %lf", &a,&b,&c);
+
+    if( a>=b && a=c )
+        printf("%.2f is the largest number.", a);
+
+    if( b<=a && b>=c )
+        printf("%.2f is the largest number.", n2);
+
+    if( n3>=n1 && n3>=n2 )
+        printf("%.2f is the largest number.", n3);
+
+    return 0;
+}
