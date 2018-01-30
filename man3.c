56ca446e4a0e9ce49209c21f74f21e755ef9c2e5
manojeminem <35653185+manojeminem@users.noreply.github.com>
2018-01-30 16:28:29 +0530

    Create man3.c

---
+++ man3.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+void main()
+{
+int n,k,a[100],i,s=0;
+clrscr();
+scanf(%d%d",&n,&k);
+for(i=0,i<n,i++)
+{
+scanf("%d",&a[i]);
+}
+for(i=0,i<k,i++)
+{
+s=s+a[i];
+}
+printf("%d",s)
+}
+getch();
+}
